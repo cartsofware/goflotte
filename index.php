31295a88b91447bbbeebe13f2cbb438d480abd95
cartsofware <assistenza@cartsoftware.eu>
2017-03-21 09:33:27 +0100

    prova di matteo index.php

--- index.php
+++ index.php
@@ -16,6 +16,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with Booked Scheduler.  If not, see <http://www.gnu.org/licenses/>.
+
+matteo 21/03/2017
 */
 
 require_once('config/config.php');
